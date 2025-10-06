efb74e71a5b76228140d6bc79c1e01260f278195
Seth Madlon-Kay <seth.madlonkay@gmail.com>
2025-10-06 14:49:35 -0400

    closer

--- sim.R
+++ sim.R
@@ -1,6 +1,14 @@
+library(data.table)
+
 n_units <- 100
 n_animals <- 3
 
+unit <- 1:(n_units*n_animals)
 animal <- rep(1:n_animals, each = n_units)
-lambda <- rgamma(n_units * n_animals, shape = 5, rate = 1)
+
+eps <- rnorm(n_units * n_animals, mean = 2, sd = 0.5)
+mu <- rnorm(n_animals-1, 0, 0.5)
+
+Z <- model.matrix(~ 0 + factor(unit) + factor(animal), data.table(unit, animal))
+lambda <- exp(Z %*% c(eps, mu))
 
